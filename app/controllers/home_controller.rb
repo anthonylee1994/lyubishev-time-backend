b208aac5bc0f889a93bc87f5a17aec4d5fc5d9f3
Anthony Lee <hososlch@gmail.com>
2024-01-01 16:32:53 +0800

    feat: rubocop lint

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
-    render json: { name: "Lyubishchev", version: "0.0.1" }
+    render json: { name: 'Lyubishchev', version: '0.0.1' }
   end
 end
