2538cc807768fb8c875a72c411948c77dc552e59
Anthony Lee <hososlch@gmail.com>
2024-01-05 18:01:23 +0800

    fix: summary

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -4,6 +4,12 @@ class SummaryController < ApplicationController
   def index
     params[:day] = 1 unless params[:day].present?
 
-    render json: @current_user.time_events.where(date: params[:day].to_i.days.ago..Time.zone.now).group(:tag_id).sum(:minute)
+    @time_events = if params[:day] == 1
+                     @current_user.time_events.where(date: Time.zone.now.all_day)
+                   else
+                     @current_user.time_events.where(date: params[:day].to_i.days.ago..Time.zone.now)
+                   end
+
+    render json: @time_events.group(:tag_id).sum(:minute)
   end
 end
