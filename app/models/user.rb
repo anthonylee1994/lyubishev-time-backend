5a9ba1d8a1d53195c6656cad95164854b6895af4
Anthony Lee <hososlch@gmail.com>
2024-01-05 21:54:46 +0800

    fix: test cese

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,13 +29,13 @@ class User < ApplicationRecord
 
   def create_default_time_event_tags
     time_event_tags.create!([
-                              { name: '緊急重要', color: Color.find_by(name: '紅色'),
+                              { name: '緊急重要', color: Color.find_or_create_by(name: '紅色', hexcode: '#ffcdd2'),
                                 order: 0 },
-                              { name: '不緊急重要', color: Color.find_by(name: '黃色'),
+                              { name: '不緊急重要', color: Color.find_or_create_by(name: '黃色', hexcode: '#fff9c4'),
                                 order: 1 },
-                              { name: '緊急不重要', color: Color.find_by(name: '綠色'),
+                              { name: '緊急不重要', color: Color.find_or_create_by(name: '綠色', hexcode: '#c8e6c9'),
                                 order: 2 },
-                              { name: '不緊急不重要', color: Color.find_by(name: '藍色'),
+                              { name: '不緊急不重要', color: Color.find_or_create_by(name: '藍色', hexcode: '#b3e5fc'),
                                 order: 3 }
                             ])
   end
