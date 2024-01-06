6dcd5482c826d0ade916f9fe8f70cdfa0bde6629
Anthony Lee <hososlch@gmail.com>
2024-01-06 10:12:54 +0800

    fix: SummaryController

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -4,7 +4,7 @@ class SummaryController < ApplicationController
   def index
     params[:day] = 1 unless params[:day].present?
 
-    @time_events = if params[:day] == 1
+    @time_events = if params[:day].to_i == 1
                      @current_user.time_events.where(date: Time.zone.today)
                    else
                      @current_user.time_events.where(date: params[:day].to_i.days.ago..Time.zone.now)
